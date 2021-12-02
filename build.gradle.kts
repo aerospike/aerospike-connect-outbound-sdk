c8f3763cff0a03c99dcf0aa4541432fbababca51
tejassp <tejas.spt@gmail.com>
2021-12-02 14:34:37 +0530

    fix gradle build file warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -158,8 +158,8 @@ allprojects {
                 val snapshotRepo = URI("https://oss.sonatype.org/content/repositories/snapshots/")
                 url = if (!isSnapshotVersion(project.version)) releaseRepo else snapshotRepo
                 credentials {
-                    username = project.properties["ossrhUsername"] as String
-                    password = project.properties["ossrhPassword"] as String
+                    username = project.properties["ossrhUsername"] as? String
+                    password = project.properties["ossrhPassword"] as? String
                 }
             }
         }
