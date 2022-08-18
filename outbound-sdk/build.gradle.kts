b07e0e2362c4f7734225d313ae9692d595613f43
Abhilash Mandaliya <abhilashmandaliya@gmail.com>
2022-08-18 16:00:50 +0530

    Updated aerospike-client version Added snyk's configuration file to ignore GPL-3.0 warning

--- outbound-sdk/build.gradle.kts
+++ outbound-sdk/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
 
     // Aerospike Java Client
-    compileOnly("com.aerospike:aerospike-client:5.1.11")
+    compileOnly("com.aerospike:aerospike-client:6.1.0")
 
     // Jackson annotations
     compileOnly(
