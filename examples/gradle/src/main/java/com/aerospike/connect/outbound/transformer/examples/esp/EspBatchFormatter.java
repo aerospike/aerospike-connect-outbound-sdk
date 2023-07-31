c7a00be4621f1e8c4eaca5fc00c00df6b0e188ee
Tejas S P <tejas.spt@gmail.com>
2023-07-31 12:39:38 +0530

    [CONNECTOR-771]] ESP, Kafka, JMS, Pulsar outbound batching examples (#7)

---
+++ examples/gradle/src/main/java/com/aerospike/connect/outbound/transformer/examples/esp/EspBatchFormatter.java
@@ -0,0 +1,78 @@
+package com.aerospike.connect.outbound.transformer.examples.esp;
+
+import com.aerospike.connect.outbound.AerospikeOperation;
+import com.aerospike.connect.outbound.esp.EspOutboundMetadata;
+import com.aerospike.connect.outbound.esp.HttpMethod;
+import com.aerospike.connect.outbound.format.BatchFormatter;
+import com.aerospike.connect.outbound.format.BatchItem;
+import com.aerospike.connect.outbound.format.DefaultTextOutboundRecord;
+import com.aerospike.connect.outbound.format.MediaType;
+import com.aerospike.connect.outbound.format.OutboundRecord;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+/**
+ * EspBatchFormatter formats a batch of change notification records into a
+ * single ESP message.
+ *
+ * <p>
+ * A snippet of a config for this formatter can be
+ * <pre>
+ * batching:
+ *   enabled: true
+ * format:
+ *   mode: custom
+ *   class: com.aerospike.connect.outbound.transformer.examples.esp.EspBatchFormatter
+ * </pre>
+ */
+public class EspBatchFormatter implements BatchFormatter<EspOutboundMetadata> {
+    private final static Logger logger =
+            LoggerFactory.getLogger(EspBatchFormatter.class.getName());
+
+    private final static ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public List<OutboundRecord<EspOutboundMetadata>> format(
+            @NonNull List<BatchItem<EspOutboundMetadata>> batchItems)
+            throws Exception {
+        Map<String, Map<String, String>> records = batchItems.stream()
+                // Filter only writes.
+                .filter(batchItem -> batchItem.getRecord().getMetadata().getOperation() == AerospikeOperation.WRITE)
+
+                // Convert to map of key to map of bins.
+                .collect(Collectors.toMap(
+                        batchItem -> batchItem.getRecord().getMetadata().getKey().toString(),
+                        batchItem -> batchItem.getRecord().getBins().entrySet().stream().collect(
+                                Collectors.toMap(Entry::getKey,
+                                        bin -> bin.getValue().toString()))
+                ));
+
+        logger.debug("Batch size {}", records.size());
+
+        if (records.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        // Convert Map of records to JSON.
+        byte[] recordsJson = objectMapper.writeValueAsBytes(records);
+
+        // Metadata will be the same for all the records in the batch.
+        EspOutboundMetadata metadata = batchItems.get(0).getFormattedRecord()
+                .getMetadata();
+
+        // Merge all the Batch items into a single record.
+        return Collections.singletonList(new DefaultTextOutboundRecord<>(
+                recordsJson, MediaType.OCTET_STREAM,
+                metadata, Collections.emptySet()
+        ));
+    }
+}
+
