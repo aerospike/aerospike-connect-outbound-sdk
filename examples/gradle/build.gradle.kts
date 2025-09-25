e171afb21f9c085bea266d1d2462c7ebb1c645c0
Abhilash Mandaliya <abhilashmandaliya@gmail.com>
2025-09-25 15:16:16 +0530

    Using latest elasticsearch outbound SDK version

--- examples/gradle/build.gradle.kts
+++ examples/gradle/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     // Aerospike outbound SDK.
     compileOnly("com.aerospike:aerospike-connect-outbound-sdk:2.2.1")
     compileOnly(
-        "com.aerospike:aerospike-connect-elasticsearch-outbound-sdk:2.1.2"
+        "com.aerospike:aerospike-connect-elasticsearch-outbound-sdk:2.1.3"
     )
     compileOnly("co.elastic.clients:elasticsearch-java:8.18.2")
 
