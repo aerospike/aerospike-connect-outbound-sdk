e719e390fa58d785ca64200285fb5def49379246
Abhilash Mandaliya <abhilashmandaliya@gmail.com>
2026-04-13 16:43:10 +0530

    Using project version of outbound-sdk in elasticsearch-outbound-sdk

--- elasticsearch-outbound-sdk/build.gradle.kts
+++ elasticsearch-outbound-sdk/build.gradle.kts
@@ -18,7 +18,7 @@
 
 dependencies {
     // Aerospike connect outbound sdk
-    api("com.aerospike:aerospike-connect-outbound-sdk:2.3.1")
+    api(project(":aerospike-connect-outbound-sdk"))
 
     // Elasticsearch client
     api("co.elastic.clients:elasticsearch-java:8.19.14")
