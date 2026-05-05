61703d25aefb8a4cd4b6e844393d035aa158b65b
Abhilash Mandaliya <abhilashmandaliya@gmail.com>
2026-05-05 14:10:54 +0530

    Aligned dependency versions used by the connectors (#64)

--- elasticsearch-outbound-sdk/build.gradle.kts
+++ elasticsearch-outbound-sdk/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     api(project(":aerospike-connect-outbound-sdk"))
 
     // Elasticsearch client
-    api("co.elastic.clients:elasticsearch-java:8.19.14") {
+    api("co.elastic.clients:elasticsearch-java:8.19.11") {
         // Exclude the Jackson 3 packages which require Java 17
         exclude("tools.jackson.core")
         exclude("tools.jackson")
